5efd726f9c9f7c9d776c819b58b419d888eb4424
jeremy <duchesne.pro@gmail.com>
2023-01-21 23:15:22 +0100

    test

--- src/controllers/utility.py
+++ src/controllers/utility.py
@@ -8,4 +8,4 @@ def absolute_path(relative_path):
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+    return os.path.join(base_path, relative_path).replace('\\', '/')
